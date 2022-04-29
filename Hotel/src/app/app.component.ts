c6a2e2f92d1cdfac4c8af04c3517eb7ba5aa704e
Santambrogio-A <santambrogio.andrea@iisgalvanimi.edu.it>
2022-04-29 16:55:10 +0000

    Punto 2

--- Hotel/src/app/app.component.ts
+++ Hotel/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Hotel';
+  obsRooms : Observable <Object> = undefined!
+  data : object = undefined!;
+  constructor(private http : HttpClient)
+  {
+
+  }
+  ngOnInit(): void {
+    this.obsRooms = this.http.get("https://my-json-server.typicode.com/malizia-g/hotel/booking")
+    this.obsRooms.subscribe(this.faiqualcosa)
+  }
+  faiqualcosa = (data : Object) => {
+    this.data = data;
+  }
 }
